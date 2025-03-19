b7b0ad69f1166bcfa1c8b312650d2490ec97ff28
Simone Silvestri <silvestri.simone0@gmail.com>
2025-03-19 20:00:54 +0100

    running version without nonlinear dynamics

--- particle_dynamics.jl
+++ particle_dynamics.jl
@@ -7,23 +7,27 @@ using StructArrays
 
 include("dynamical_particles.jl")
 
-Lx =2π
-Ly =2π
+Lx = 2π
+Ly = 2π
 
 grid = RectilinearGrid(size = (200, 200),
                        x = (-Lx, Lx),
                        y = (-Ly, Ly),
                        topology = (Periodic, Periodic, Flat))
 
-ψ=Field{Face,Face,Center}(grid)
-ψi(x,y)=2 * (cos(x)+cos(y))
-set!(ψ,ψi)
+ψ = Field{Face, Face, Center}(grid)
+
+ψi(x, y) =2 * (cos(x)+cos(y))
+
+set!(ψ, ψi)
+
 Oceananigans.BoundaryConditions.fill_halo_regions!(ψ)
-u=-∂y(ψ)
-v= ∂x(ψ)
+u = - ∂y(ψ)
+v =   ∂x(ψ)
 
 u = compute!(Field(u))
 v = compute!(Field(v))
+
 Oceananigans.BoundaryConditions.fill_halo_regions!((u,v))
 
 using GLMakie
@@ -38,8 +42,8 @@ u=Float64[0 for p in 1:NP]
 v=Float64[0 for p in 1:NP]
 w=Float64[0 for p in 1:NP]
 
-δ=Float64[1 for p in 1:NP]
-τ=Float64[1 for p in 1:NP]
+δ=Float64[rand(Float64) for p in 1:NP]
+τ=Float64[1             for p in 1:NP]
 
 particle_struct = StructArray{DynamicalParticle}((x, y, z, u, v, w, δ, τ))
 
@@ -78,15 +82,40 @@ model=HydrostaticFreeSurfaceModel(;grid,
                                   velocities=PrescribedVelocityFields(;u,v),
                                   particles=LagrangianParticles(particle_struct; dynamics=nonlinear_dynamics))
 
-particle_sim=Simulation(model,Δt=0.001,stop_time=100)
+particle_sim=Simulation(model, Δt=0.001, stop_time=60)
 
 progress(sim) = @info "Time: ", Oceananigans.Utils.prettytime(sim.model.clock.time) 
-add_callback!(particle_sim, progress, IterationInterval(1000))
+add_callback!(particle_sim, progress, IterationInterval(100))
+
+particle_sim.output_writers[:particles] = JLD2Writer(model, (; p = model.particles),
+                                                     filename = "particles.jld2",
+                                                     schedule = IterationInterval(10),
+                                                     overwrite_existing = true)
 
 run!(particle_sim)
 
 contourf(ψ)
 scatter!(model.particles.properties.x,model.particles.properties.y)
 
+file    = jldopen("particles.jld2")
+iter    = Observable(1)
+indices = keys(file["timeseries/t"])
 
+xp = @lift(file["timeseries/p/" * indices[$iter]].x)
+yp = @lift(file["timeseries/p/" * indices[$iter]].y)
+δp = @lift(file["timeseries/p/" * indices[$iter]].δ)
+up = @lift(file["timeseries/p/" * indices[$iter]].u)
+vp = @lift(file["timeseries/p/" * indices[$iter]].v)
+sp = @lift($up.^2 + $vp.^2)
 
+fig = Figure()
+ax  = Axis(fig[1, 1])
+contourf!(ψ, colormap = :greys)
+scatter!(xp, yp, color = δp, colormap = :magma)
+
+record(fig, "particle_video.mp4", 1:length(indices)) do i
+    @info "recording $i of $(length(indices))"
+    iter[] = i
+end
+
+close(file)
